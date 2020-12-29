c01e9e45ccc72ff5211274462d48ffcbf77bb623
hmountainn <ghkdwltks123@naver.com>
2020-12-29 18:43:09 +0900

    <jsp20-1: 서블릿필터> - 어노테이션사용 버전 1.filter파일 생성해서 인코딩설정 2.어노테이션으로 filter사용설정

---
+++ jspPJ/src/com/jisan/web/filter/CharacterEncodingFliter.java
@@ -0,0 +1,25 @@
+package com.jisan.web.filter;
+
+import java.io.IOException;
+
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.annotation.WebFilter;
+
+@WebFilter("/*") //어노테이션으로 필터 사용설정
+public class CharacterEncodingFliter implements Filter {
+
+	@Override
+	public void doFilter(ServletRequest req, ServletResponse resp, FilterChain chain)
+			throws IOException, ServletException {
+		
+		
+		req.setCharacterEncoding("UTF-8"); // 입력받은거 UTF-8로 처리
+		chain.doFilter(req,resp); //필터거치고 다음거 실행
+
+	}
+
+}
